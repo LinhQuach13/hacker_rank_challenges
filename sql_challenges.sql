cda6ae4ca66d1c3647112b43d20abd9ac8a4069a
Linh Quach <Linh.M.Quach1@gmail.com>
2021-09-09 21:08:35 -0500

    adding sql challenges from hackerrank

---
+++ sql_challenges.sql
@@ -0,0 +1,10 @@
+---Query all attributes in the CITY table.---
+SELECT * FROM CITY;
+
+--- Query attributes the CITY table. The ID for 1661.---
+SELECT * FROM CITY
+WHERE ID = 1661;
+
+---Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.---
+SELECT * FROM CITY
+WHERE COUNTRYCODE = 'JPN';
