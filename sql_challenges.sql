9647d55f6a1d20eeb2a56a001290eb672be750df
Linh Quach <Linh.M.Quach1@gmail.com>
2021-09-10 16:29:38 -0500

    updated

--- sql_challenges.sql
+++ sql_challenges.sql
@@ -25,3 +25,8 @@ WHERE (ID % 2) = 0;
 --- Find the difference between the total number of CITY entries in the table and the number of 
 -- distinct CITY entries in the table.
 SELECT COUNT(CITY) - COUNT(DISTINCT CITY) FROM STATION; 
+
+---  Query all columns for all American cities in the CITY table with populations larger than 100000. 
+--- The CountryCode for America is USA.
+SELECT * FROM CITY
+WHERE COUNTRYCODE = 'USA' AND population > 100000;
