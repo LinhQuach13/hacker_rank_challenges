112c85913ea2e21aeb41d0eb86ddd43ffb5ffd7b
Linh Quach <Linh.M.Quach1@gmail.com>
2021-09-15 20:32:43 -0500

    updated

--- sql_challenges.sql
+++ sql_challenges.sql
@@ -1,3 +1,5 @@
+-------------EASY--------------
+
 ---Query all attributes in the CITY table.---
 SELECT * FROM CITY;
 
@@ -30,3 +32,11 @@ SELECT COUNT(CITY) - COUNT(DISTINCT CITY) FROM STATION;
 --- The CountryCode for America is USA.
 SELECT * FROM CITY
 WHERE COUNTRYCODE = 'USA' AND population > 100000;
+
+
+
+-------------MEDIUM-----------------------
+--- Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE = 'USA' AND POPULATION > 120000;
