59586b3015c44580181c11092dafcdd9add0b6b1
Linh Quach <Linh.M.Quach1@gmail.com>
2021-09-16 15:38:24 -0500

    updated

--- sql_challenges.sql
+++ sql_challenges.sql
@@ -40,3 +40,9 @@ WHERE COUNTRYCODE = 'USA' AND population > 100000;
 SELECT NAME
 FROM CITY
 WHERE COUNTRYCODE = 'USA' AND POPULATION > 120000;
+
+-- Query the two cities in STATION with the shortest and longest CITY names, as well as 
+-- their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, 
+-- choose the one that comes first when ordered alphabetically.
+SELECT CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY), CITY LIMIT 1;
+SELECT CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY) DESC, CITY LIMIT 1;
