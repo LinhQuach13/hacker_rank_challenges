d44af79dd57bdad8cb7568bc858592e06ee5171f
Linh Quach <Linh.M.Quach1@gmail.com>
2021-09-17 17:36:20 -0500

    update

--- sql_challenges.sql
+++ sql_challenges.sql
@@ -48,8 +48,15 @@ SELECT CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY), CITY LIMIT 1;
 SELECT CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY) DESC, CITY LIMIT 1;
 
 
---- Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+--- Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. 
+-- Your result cannot contain duplicates.
 
 SELECT DISTINCT CITY 
 FROM STATION 
 WHERE CITY LIKE 'A%' OR CITY LIKE 'E%' OR CITY LIKE 'I%' OR CITY LIKE 'O%' OR CITY LIKE 'U%';
+
+--- Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. 
+--- Your result cannot contain duplicates.
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY LIKE '%A' OR CITY LIKE '%E' OR CITY LIKE '%I' OR CITY LIKE '%O' OR CITY LIKE '%U';
