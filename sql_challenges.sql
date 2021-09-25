aef2ad151858b2ec122a947644a52e847680230f
Linh Quach <Linh.M.Quach1@gmail.com>
2021-09-25 16:06:24 -0500

    updated

--- sql_challenges.sql
+++ sql_challenges.sql
@@ -78,3 +78,18 @@ Where CITY NOT LIKE 'A%' AND CITY NOT LIKE 'E%' AND CITY NOT LIKE 'I%' AND CITY 
 SELECT DISTINCT CITY
 FROM STATION
 WHERE NOT(CITY LIKE '%a' OR CITY LIKE '%e' OR CITY LIKE '%i' OR CITY LIKE '%o' OR CITY LIKE '%u'); 
+
+---Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. 
+-- Your result cannot contain duplicates.
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE (CITY NOT LIKE 'a%'
+        AND CITY  NOT LIKE 'e%'
+        AND CITY NOT LIKE 'i%' 
+        AND CITY NOT LIKE 'o%'
+        AND CITY NOT LIKE 'u%')
+OR (CITY NOT LIKE '%a' AND
+    CITY  NOT LIKE '%e' AND
+    CITY NOT LIKE '%i' AND 
+    CITY NOT LIKE '%o' AND 
+    CITY NOT LIKE '%u');
