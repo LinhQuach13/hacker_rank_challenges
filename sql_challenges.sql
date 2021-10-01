7b606545e5d6b099920f6bdd839db87510a8e8d9
Linh Quach <Linh.M.Quach1@gmail.com>
2021-10-01 12:02:56 -0500

    updated

--- sql_challenges.sql
+++ sql_challenges.sql
@@ -115,3 +115,10 @@ ORDER BY substr(name, -3, 3), id;
 SELECT name
 FROM Employee 
 ORDER BY name;
+
+-- Write a query that prints a list of employee names (i.e.: the name attribute) for 
+--- employees in Employee having a salary greater than  per month who have been employees for less than  months. Sort your result by ascending employee_id.
+SELECT name
+FROM Employee
+WHERE salary > 2000 and months < 10
+ORDER BY employee_id asc;
