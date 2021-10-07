acff26fbebb9993a60dea63edb1518c0f851eb8c
Linh Quach <Linh.M.Quach1@gmail.com>
2021-10-07 17:05:44 -0500

    updated

--- sql_revising_aggregations.sql
+++ sql_revising_aggregations.sql
@@ -17,3 +17,8 @@ WHERE DISTRICT = 'California';
 --- Query the average population for all cities in CITY, rounded down to the nearest integer.
 SELECT ROUND(AVG(POPULATION), 0)
 FROM CITY;
+
+--- Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN.
+SELECT SUM(POPULATION)
+FROM CITY
+WHERE COUNTRYCODE = 'JPN';
