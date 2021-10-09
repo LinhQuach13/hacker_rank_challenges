1b72c7a188fef3a6341fdd75c73103a878c28a6d
Linh Quach <Linh.M.Quach1@gmail.com>
2021-10-09 17:05:15 -0500

    adding python challenges

---
+++ python_challenges.py
@@ -0,0 +1,44 @@
+# Task 
+# print 'Hello World'
+# Input Format
+# You do not need to read any input in this challenge.
+#Output Format
+# Print Hello, World! to stdout.
+print("Hello, World!")
+
+
+
+
+# Task 
+# Given an integer, , perform the following conditional actions:
+# If  is odd, print Weird
+# If  is even and in the inclusive range of  to , print Not Weird
+# If  is even and in the inclusive range of  to , print Weird
+# If  is even and greater than , print Not Weird
+
+# Input Format
+# A single line containing a positive integer, .
+
+# Constraints
+# 1 <= n <= 100
+
+# Output Format
+# Print Weird if the number is weird; otherwise, print Not Weird.
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+if n % 2 != 0:
+    print("Weird")
+elif n % 2 == 0 and 2 <= n <= 5:
+    print("Not Weird")
+elif n % 2 == 0 and 6 <= n <= 20:
+    print("Weird")
+else:
+    print("Not Weird")
