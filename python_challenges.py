343463addc91fbd25684ee1d20b64e725282d93e
Linh Quach <Linh.M.Quach1@gmail.com>
2021-10-11 13:09:03 -0500

    updated

--- python_challenges.py
+++ python_challenges.py
@@ -42,3 +42,18 @@ elif n % 2 == 0 and 6 <= n <= 20:
     print("Weird")
 else:
     print("Not Weird")
+
+
+# Task
+# The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+# The first line contains the sum of the two numbers.
+# The second line contains the difference of the two numbers (first - second).
+# The third line contains the product of the two numbers.
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+print (a + b)
+print (a - b)
+print (a * b)
