9f304c51d14726e3673025d6b1275a2f54083778
Linh Quach <Linh.M.Quach1@gmail.com>
2021-10-18 13:41:50 -0500

    updated

--- python_challenges.py
+++ python_challenges.py
@@ -95,12 +95,14 @@ if __name__ == '__main__':
 # Note that the code stub provided reads from STDIN and passes arguments to the is_leap function. It is only necessary to complete the is_leap function.
 def is_leap(year):
     leap = False
-    if year/400 == 0:
+    
+    if year % 400 == 0:
         leap = True
-    elif year/100 == 0:
+    elif year % 100 == 0:
         leap = False
-    elif year/4 == 0:
+    elif year % 4 == 0:
         leap = True
+    
     return leap
 
 year = int(input())
