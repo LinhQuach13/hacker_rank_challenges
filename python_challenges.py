280a76a8678886d11ecb53a13fbc176f99385ad4
Linh Quach <Linh.M.Quach1@gmail.com>
2021-10-18 13:51:10 -0500

    updated

--- python_challenges.py
+++ python_challenges.py
@@ -106,3 +106,36 @@ def is_leap(year):
     return leap
 
 year = int(input())
+
+
+# Task
+# The included code stub will read an integer, n , from STDIN.
+
+# 123...n
+
+# Without using any string methods, try to print the following:
+
+
+# Note that "" represents the consecutive values in between.
+
+# Example
+# n= 5
+
+# Print the string 12345.
+
+# Input Format
+
+# The first line contains an integer .
+
+# Constraints
+# 1<= n <= 150
+
+# Output Format
+
+# Print the list of integers from 1 through n as a string, without spaces.
+if __name__ == '__main__':
+    n = int(input())
+    #for loop for iteration
+    #stepwise with a range
+for i in range(n):
+    print(i+1, end="")
