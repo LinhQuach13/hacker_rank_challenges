c86771805446930b9f748ca31ccdd3194d7e4ea9
Linh Quach <Linh.M.Quach1@gmail.com>
2021-10-20 12:43:45 -0500

    updated

--- sql_revising_aggregations.sql
+++ sql_revising_aggregations.sql
@@ -45,3 +45,14 @@ SELECT CONCAT('There are a total of ', COUNT(Occupation), ' ', LOWER(Occupation)
 FROM OCCUPATIONS 
 GROUP BY Occupation 
 ORDER BY COUNT(Occupation), Occupation;
+
+-- Task
+-- Samantha was tasked with calculating the average monthly salaries for all employees in the EMPLOYEES table, 
+-- but did not realize her keyboard's  key was broken until after completing the calculation. She wants your help 
+-- finding the difference between her miscalculation (using salaries with any zeros removed), and the actual average salary.
+
+-- Write a query calculating the amount of error (i.e.: actual - miscalculated average monthly salaries), 
+-- and round it up to the next integer.
+SELECT 
+    ROUND(AVG(SALARY)) - ROUND(AVG(REPLACE(SALARY, '0','')))
+FROM EMPLOYEES;
