4eb6bc0ea0d486cd69900582f2db8e9ded4b4feb
Linh Quach <Linh.M.Quach1@gmail.com>
2021-12-31 16:35:33 -0600

    updated

--- sql_revising_aggregations.sql
+++ sql_revising_aggregations.sql
@@ -76,7 +76,10 @@ SELECT ROUND(SUM(LAT_N), 2), ROUND(SUM(LONG_W), 2)
 FROM STATION;
 
 
--- Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than  and less than . Truncate your answer to 4 decimal places.
+-- Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 137.2345. Truncate your answer to 4 decimal places.
+SELECT ROUND(SUM(LAT_N), 4)
+FROM STATION
+WHERE LAT_N > 38.7880 AND LAT_N < 137.2345;
 
 
 
